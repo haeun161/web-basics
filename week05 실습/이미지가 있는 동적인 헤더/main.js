f1278ec308639166cdbfa6929b218f7e8373bc00
이하은 <gracelee161@gmail.com>
2021-04-07 23:55:03 +0900

    week5 pratice refixed

--- week05 실습/이미지가 있는 동적인 헤더/main.js
+++
@@ -1,8 +0,0 @@
-//햄버거 바 선택자
-const openMenu=document.getElementById("btn--open")
-// 닫는 아이콘 선택자
-const openMenu=document.getElementById("btn--close")
-// 사이드 메뉴 선택자
-const openMenu=document.getElementById("nav_menu")
-
-openMenuEL.addEventListener("click", function)

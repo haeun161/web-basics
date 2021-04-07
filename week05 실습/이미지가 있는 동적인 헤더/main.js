e79d56fce5d1802afca63b81c201a6e95cac270b
이하은 <gracelee161@gmail.com>
2021-04-07 17:31:03 +0900

    햄버거가 있는 헤더

---
+++ week05 실습/이미지가 있는 동적인 헤더/main.js
@@ -0,0 +1,8 @@
+//햄버거 바 선택자
+const openMenu=document.getElementById("btn--open")
+// 닫는 아이콘 선택자
+const openMenu=document.getElementById("btn--close")
+// 사이드 메뉴 선택자
+const openMenu=document.getElementById("nav_menu")
+
+openMenuEL.addEventListener("click", function)
